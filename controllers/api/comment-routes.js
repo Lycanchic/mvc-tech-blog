3ea02244cdf5804fd043f52ffcc86bc0ffbdfbed
Marie P <map40j@gmail.com>
2023-02-17 19:29:13 -0800

    Began working on the routes/comment, added the router.post 1st version

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,19 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/Auth');
 
+router.post('/', withAuth, async (req, res) => {
+    const { body, session } = req;
+    try {
+      const newComment = await Comment.create({
+        ...body,
+        userId: session.userId,
+      });
+      res.status(201).json(newComment);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'An error occured' });
+    }
+  });
+
+  
 module.exports = router
