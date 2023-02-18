0be5b2e65f9cc8f3fd508d375d4386d61212ae33
Marie P <map40j@gmail.com>
2023-02-17 19:47:58 -0800

    Completed the routes, index.js 1st version

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const express = require('express');
+const userRoutes = require('./userRoutes');
+
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
+
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+
+router.use('./user', userRoutes);
+
+
+module.exports = router;
