1d5c08cf1c04ed56662865e2f78911057d0697e8
Marie P <map40j@gmail.com>
2023-02-18 16:04:04 -0800

    fixing bug issues

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
-const express =require('express');
-const session =require('express-session');
-const exphbs =require('express-handlebars');
-const helpers =require('./utils/helpers');
-
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('utils/helpers');
+ 
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
